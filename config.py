1b525486e08f9d5b809a3299fb996fd679176b86
owenkuai <owenkuai@gmail.com>
2019-07-03 19:33:54 +0800

    updae config

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'owen-is-best' )
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.partner.outlook.cn')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
         ['true', 'on', '1']
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME', "dcs@daocloud.io")
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD', "Danger0us123")
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Mr.MagicMonkey]'
+    FLASKY_MAIL_SENDER = 'Mr.MagicMonkey <dcs@daocloud.io>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN', 'owen')
     SSL_REDIRECT = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
