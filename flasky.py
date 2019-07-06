48e4ab93e1924580a59478bf0067ad435f406518
owenkuai <owenkuai@gmail.com>
2019-07-06 21:51:39 +0800

    test:

--- flasky.py
+++ flasky.py
@@ -11,11 +11,13 @@ if os.environ.get('FLASK_COVERAGE'):
     COV = coverage.coverage(branch=True, include='app/*')
     COV.start()
 
+
 import sys
 import click
 from flask_migrate import Migrate, upgrade
 from app import create_app, db
 from app.models import User, Follow, Role, Permission, Post, Comment
+
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
